d92b7d4375b66c779b12c34740baf0c4fc7531e4
jelius <edynamite05@gmail.com>
2026-06-01 12:13:19 +0300

    lkj

---
+++ for_loop.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// function main begins program execution
+int main(void) {
+    for (int counter = 1; counter <= 100; counter++) {
+        printf("%d ", counter);
+    }
+    puts(""); // output a newline
+}
