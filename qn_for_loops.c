fdbb2fe5933d3a16c893b65aa6719f30d9d43e41
jelius <edynamite05@gmail.com>
2026-06-01 13:03:59 +0300

    oo

--- qn_for_loops.c
+++ qn_for_loops.c
@@ -1,15 +1,25 @@
-
-
+// Calculating compound interest.
 #include <stdio.h>
 #include <math.h>
-// function main begins program execution
-int main (void) {
-    double principal =1000.00; // initial amount invested
-    double rate = 0.05; // annual interest rate
-    printf ("%4s%21f", "Year", "Amount on deposit\n");
-    for (int year =1; year<=10; year++)
-    {
-        double amount = principal * pow(1.0 + rate, year);
-        printf("%4d%21.2f\n", year, amount);
-    }
+
+int main(void) {
+
+double principal = 1000.0; //starting principal
+double rate = 0.05; // annual interest rate
+
+// output table column heads
+printf("%4s%21s\n", "Year", "Amount on deposit");
+// calculate amount on deposit for each of ten years
+
+for (int year = 1; year <= 10; ++year) {
+
+// calculate new amount for specified year
+
+double amount = principal * pow(1.0 + rate,
+year);
+
+// output one table row
+
+printf("%4d%21.2f\n", year, amount);
+}
 }
