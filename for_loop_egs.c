a2b6e080e685e55bfeeb8bcac74b9dee46489a73
jelius <edynamite05@gmail.com>
2026-06-01 12:24:11 +0300

    jj

---
+++ for_loop_egs.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// function main begins program execution
+int main (void) {
+    for (int i = 20; i >= 2; i -= 2) {
+        printf("%d",i);
+    }
+puts (""); // output a newline
+}
