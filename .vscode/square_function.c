26d518664576165eb8d09d98554caa953b651c98
jelius <edynamite05@gmail.com>
2026-06-01 16:10:45 +0300

    pp

---
+++ .vscode/square_function.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int square (int number);
+int main (void) {
+    for (int x=1;x<=10;++x)
+    printf("%d",square(x));
+puts("");
+int square(int number) {
+    return number * number;   
+}
+}
